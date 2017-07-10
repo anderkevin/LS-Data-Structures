aff3a252c1d341da939ff36224bc4cb706fc5018
Kevin <anderkevin@gmail.comgit config --global user.name anderkevingit config --global user.email anderkevin@gmail.com>
2017-07-10 16:20:01 -0600

    Stack completed?

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,13 @@
 class Stack {
-
+  const add () => {
+    add.push();
+  };
+  const remove () => {
+  return remove.pop();
+  };
+  get size () {
+    return this.size = array.length;
+  };
 }
 
 module.exports = Stack;
