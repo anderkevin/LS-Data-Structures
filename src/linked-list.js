74b71d9dc490b867306ba331d3b21103ee6151d8
jeshsy <jeshsy@users.noreply.github.com>
2017-07-11 16:30:20 -0400

    got linked list to pass first test only. dunno how to proceed

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,21 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(stuff) {
+    const thingtoadd = {
+      stuff,
+      next: null
+    };
+    if (this.head === null) this.head = thingtoadd;
+    this.tail.next = thingtoadd;
+    this.tail = thingtoadd;
+  }
+  removeHead() {
+    this.head = this.head.next;
+  }
+  contains() {
+    return this.head;
+  }
 }
 
 module.exports = LinkedList;
