73ad334c0855657ec9723f4aa08903322a82d7d0
jeshsy <jeshsy@users.noreply.github.com>
2017-07-11 13:39:58 -0400

    queue passed all the tests

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,18 @@
 class Queue {
-
+  constructor() {
+    this.thequeue = [];
+  }
+  enqueue(value) {
+    this.thequeue.unshift(value);
+  }
+  dequeue() {
+    if (this.thequeue.length === 0) return null;
+    return this.thequeue.pop();
+  }
+  get size() {
+    return this.thequeue.length;
+  }
 }
+
 
 module.exports = Queue;
