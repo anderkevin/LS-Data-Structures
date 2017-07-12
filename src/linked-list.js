146ac61a36197abad3791e21c0cd0e0c6a11c7f5
anderkevin <anderkevin@gmail.com>
2017-07-12 11:19:51 -0600

    3 tests passed on linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,19 +5,28 @@ class LinkedList {
     // Do not modify anything inside of the constructor
   }
   addToTail(stuff) {
-    const thingtoadd = {
-      stuff,
+    const node = {
+      value: stuff,
       next: null
     };
-    if (this.head === null) this.head = thingtoadd;
-    this.tail.next = thingtoadd;
-    this.tail = thingtoadd;
+    if (this.head === null) this.head = node;
+    else {
+      this.head.next = node.value;
+    }
+    this.tail = node;
   }
   removeHead() {
+    const originalHead = this.head;
     this.head = this.head.next;
+    return originalHead;
   }
   contains() {
-    return this.head;
+    return this.tail;
+    // for (let i = 0; i < this.length; i++) {
+      // if () {
+        // return true;
+      // }
+    // }
   }
 }
 
